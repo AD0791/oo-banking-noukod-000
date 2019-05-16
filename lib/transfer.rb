aff13ce13a083d6b0a101b1c80c8826479ba8067
Alexandro Disla <alexandrodisla@hotmail.com>
2019-05-16 19:40:05 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,36 +29,6 @@ class Transfer
   end
   
   def reverse_transfert
-    class Transfer
-  attr_accessor :sender, :receiver, :amount, :status
-  
-  def initialize(sender, receiver, amount)
-    @sender = sender
-    @receiver = receiver
-    @amount = amount
     
-    @status = "pending"
-  end
-  
-  def valid?
-    sender.valid? && receiver.valid?
-  end
-  
-  def execute_transaction
-    if valid?  && sender.balance > amount && @status == "complete"
-      reveiver.balance -= amount
-      sender.balance += amount
-      @status = "reversed"
-    else
-      reject_transert
-    end
-  end
-  
-  def reject_transert
-    @status = "rejected"
-    "Transaction rejected. Please check your account balance."
-  end
-end
-
   end
 end
